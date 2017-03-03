6826d265705d00d95658db9e6a74c4659fb56bd6
Evgeniy Poznyak <ek35mm@gmial.com>
2017-03-02 23:38:23 -0600

    Initial commit

---
+++ src/SeaBattle.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+/**
+ * Created by E&K PC on 3/2/2017.
+ */
+public class SeaBattle {
+
+        public static void main(String[] args) {
+            char[] cells = new char[10];
+
+            for (int i = 0; i < cells.length; i++) {
+                cells[i] = '.';
+            }
+            int position = 4;
+            cells[position] = 'X';
+
+/*        for (char cell : cells) {
+            System.out.print(cell);
+        }*/
+
+            do {
+                System.out.println(cells);
+                Scanner scanner = new Scanner(System.in);
+                String s;
+                s = scanner.nextLine();
+                System.out.printf("Вы ввели: %s\n", s);
+
+                int shoot = Integer.parseInt(s);
+
+                switch (cells[shoot]) {
+                    case '.':
+                        System.out.println("Промах!");
+                        cells[shoot] = '*';
+                        break;
+                    case 'X':
+                        System.out.println("Супер! Корабль потоплен!");
+                        cells[shoot] = '-';
+                        System.out.println(cells);
+                        break;
+                    case '*':
+                        System.out.println("Уже стреляли!");
+                        break;
+                    default:
+                        System.out.println("ERROR!");
+
+                }
+
+            } while (cells[position] == 'X');
+
+
+        }
+    }
+
+
